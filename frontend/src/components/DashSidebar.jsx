113f138d1f236c930ff11bd76b7cec25a440af0e
maui30 <103097103+maui30@users.noreply.github.com>
2024-03-18 23:54:12 +0800

    sidebar for dashboard

---
+++ frontend/src/components/DashSidebar.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+
+import { useDispatch } from "react-redux";
+
+import { Sidebar } from "flowbite-react";
+import { HiUser, HiLogout } from "react-icons/hi";
+import { signOutSuccess } from "../redux/users/userSlice";
+
+const DashSidebar = () => {
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+
+    if (tabFromUrl) setTab(tabFromUrl);
+    console.log(tabFromUrl);
+  }, [location.search]);
+
+  const handleSignOut = async () => {
+    const res = await fetch("/api/users/signout", {
+      method: "POST",
+    });
+
+    const data = await res.json();
+
+    if (!res.ok) {
+      console.log(data.message);
+    } else {
+      dispatch(signOutSuccess());
+    }
+  };
+
+  return (
+    <Sidebar className="w-full md:w-56">
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          <Link to="/Dashboard?tab=profile">
+            <Sidebar.Item
+              active={tab === "profile"}
+              icon={HiUser}
+              label={"User"}
+              labelColor="dark"
+            >
+              Profile
+            </Sidebar.Item>
+          </Link>
+
+          <Sidebar.Item
+            icon={HiLogout}
+            labelColor="dark"
+            onClick={handleSignOut}
+          >
+            Sign Out
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  );
+};
+
+export default DashSidebar;

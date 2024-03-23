e7006f6825446cd5bbef30771a2f2647035774db
maui30 <103097103+maui30@users.noreply.github.com>
2024-03-23 15:38:15 +0800

    post create

--- backend/controller/postController.js
+++ backend/controller/postController.js
@@ -2,9 +2,33 @@ const Post = require("../models/Post");
 const asyncHandler = require("express-async-handler");
 
 const createPost = asyncHandler(async (req, res) => {
-  const { userId, title, content, image, category, slug } = req.body;
+  const { title, content, image, category } = req.body;
 
   if (!title || !content) {
     return res.status(400).json({ message: "Title and Content is required" });
   }
+
+  const slug = title
+    .split(" ")
+    .join("-")
+    .replace(/[^a-zA-Z0-9-]/g, "");
+
+  const postObj = {
+    userId: req.user.id,
+    title,
+    content,
+    image,
+    category,
+    slug,
+  };
+
+  const post = await Post.create(postObj);
+
+  if (post) {
+    res.status(200).json({ ...post, message: "Post successfully created" });
+  } else {
+    res.status(400).json({ message: "Invalid Post" });
+  }
 });
+
+module.exports = { createPost };
--- backend/routes/postsRoute.js
+++ backend/routes/postsRoute.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const postController = require("../controller/postController");
+const verifyJWT = require("../utils/verifyJWT");
 
-router.route("/");
+router.route("/").post(verifyJWT, postController.createPost);
 
 module.exports = router;

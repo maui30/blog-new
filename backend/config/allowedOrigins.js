69b58aa2045e7c66c0b5b3d3e8ba54cf59f5b9ed
maui30 <103097103+maui30@users.noreply.github.com>
2024-04-24 15:18:22 +0800

    added origin

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,3 +1,7 @@
-const allowedOrigins = ["https://localhost:3500", "http://localhost:5173"];
+const allowedOrigins = [
+  "https://localhost:3500",
+  "http://localhost:5173",
+  "https://blog-mern-42ci.onrender.com",
+];
 
 module.exports = allowedOrigins;
